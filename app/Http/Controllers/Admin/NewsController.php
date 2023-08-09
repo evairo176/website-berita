6b9acdd1caf4e875274338a31cd56fd2752a6c2a
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-08-09 09:12:47 +0700

    feat: add tags table & news_tags table with relationship

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\AdminNewsCreateRequest;
 use App\Models\Category;
 use App\Models\Language;
 use App\Models\News;
+use App\Models\Tag;
 use App\Traits\FileUploadTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -64,6 +65,21 @@ class NewsController extends Controller
         $news->show_at_popular = $request->show_at_popular == 1 ? 1 : 0;
         $news->status = $request->status == 1 ? 1 : 0;
         $news->save();
+
+        $tags = explode(',', $request->tags);
+        $tagIds = [];
+
+        foreach ($tags as $tag) {
+            $item = new Tag();
+
+            $item->name = $tag;
+            $item->save();
+            $tagIds[] = $item->id;
+        }
+
+        $news->tags()->attach($tagIds);
+
+
 
         toast(__('Created successfully'), 'success')->width("350");
 
