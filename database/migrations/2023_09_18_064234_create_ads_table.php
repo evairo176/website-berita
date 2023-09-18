2f21ce4a97c05e3d6cccd5cbf587ce336cb649a3
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-09-18 17:05:49 +0700

    update: add url to every ads

--- database/migrations/2023_09_18_064234_create_ads_table.php
+++ database/migrations/2023_09_18_064234_create_ads_table.php
@@ -14,14 +14,19 @@ return new class extends Migration
         Schema::create('ads', function (Blueprint $table) {
             $table->id();
             $table->text('home_top_bar_ads');
+            $table->text('home_top_bar_ads_url')->nullable();
             $table->boolean('home_top_bar_ads_status');
             $table->text('home_middle_ads');
+            $table->text('home_middle_ads_url')->nullable();
             $table->boolean('home_middle_ads_status');
             $table->text('news_page_ads');
+            $table->text('news_page_ads_url')->nullable();
             $table->boolean('news_page_ads_status');
             $table->text('view_page_ads');
+            $table->text('view_page_ads_url')->nullable();
             $table->boolean('view_page_ads_status');
             $table->text('sidebar_ads');
+            $table->text('sidebar_ads_url')->nullable();
             $table->boolean('sidebar_ads_status');
             $table->timestamps();
         });
